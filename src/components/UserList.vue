517498bc890e30cd9a981c7d9cc47c37ff89fdc5
xuzhihao <774910945@qq.com>
2020-06-27 10:39:11 +0800

    my

---
+++ src/components/UserList.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <v-list dense>
+          <v-list-item link>
+            <v-list-item-action>
+              <v-icon>mdi-view-dashboard</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>Dashboard</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item link>
+            <v-list-item-action>
+              <v-icon>mdi-cog</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>Settings</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/MainChat.vue
@@ -0,0 +1,59 @@
+<template>
+    <div>
+    <div id="app">
+  <v-app id="inspire">
+    <v-app id="inspire">
+      <v-navigation-drawer
+        v-model="drawer"
+        app
+        clipped>
+        <user-list></user-list>
+      </v-navigation-drawer>
+      <v-app-bar
+        app
+        clipped-left>
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+        <v-toolbar-title>我的聊天室</v-toolbar-title>
+      </v-app-bar>
+  
+      <v-main>
+        <v-container
+          class="fill-height"
+          fluid>
+
+        </v-container>
+      </v-main>
+              <v-footer app>
+            <span>&copy; 2020</span>
+      </v-footer>
+      <Footer></Footer>
+    </v-app>
+  </v-app>
+</div>
+    </div>
+</template>
+
+<script>
+import UserList from '../components/UserList'
+import Footer from '../components/Footer'
+export default {
+  name: 'App',
+   props: {
+    source: String,
+  },data: () => ({
+    drawer: null,
+  }),
+  created () {
+    this.$vuetify.theme.dark = true
+  },
+
+  components: {
+    UserList,
+    Footer
+  },
+};
+</script>
+
+<style>
+
+</style>
